b318ed328ce39284f94dc024ed7d571879ef7e7d
Mozzey <mbcacciano@gmail.com>
2018-06-09 18:41:57 -0400

    GC Unit 1 Date Difference Task

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DifferenceBetweenDates
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            // Keep the program running until the user decided to exit
+			bool isRunning = true;
+			while (isRunning)
+			{
+				// Greet the user and explain the program
+				Console.WriteLine("Welcome to the Date difference Program!");
+				Console.WriteLine("Please enter a start and end Date and I will calculate the difference.");
+				Console.WriteLine("Example: dd/mm/yyyy (optional) hh:mm.");
+                Console.WriteLine();
+				// Store the two user inputs and convert them to DateTime
+				Console.Write("Start Date : ");
+				var startDate = Console.ReadLine();
+				Console.Write("End Date : ");
+				var endDate = Console.ReadLine();
+                Console.WriteLine();
+                // Run the dateDifference method you get the exact time between two dates
+				dateDifference(startDate, endDate);
+				
+                Console.WriteLine();
+				Console.WriteLine("Would you like to run the program again? Y / N");
+				string runAgain = Console.ReadLine();
+				if (!(runAgain == "Y" || runAgain == "y"))
+				{
+					isRunning = false;
+				}
+
+			}
+		}
+
+		private static void dateDifference(string startDate, string endDate) 
+		{
+            // Convert user input into DateTime
+            DateTime startDateConv;
+            DateTime endDateConv;
+            if (DateTime.TryParse(endDate, out endDateConv) && DateTime.TryParse(startDate, out startDateConv))
+	        {
+                TimeSpan timeDifference = endDateConv - startDateConv;
+                // Store TimeSpan properties to make string formatting more readable				
+			    int days = timeDifference.Days;
+			    int hours = timeDifference.Hours;
+			    int minutes = timeDifference.Minutes;
+                // Formatted string to display to the user
+			    string timeDiff = String.Format("{0} Days {1} Hours {2} Minutes", days, hours, minutes);
+			    Console.WriteLine(timeDiff);
+	        }
+            else
+	        {
+                Console.WriteLine("Invalid Date Format...Please Try Again!");
+	        }
+		}
+	}
+}
